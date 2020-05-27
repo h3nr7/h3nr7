dee357cd757c1fd4315f1b05a2595b2230e368d8
Henry YP Ho <henryyp@monkiki.co>
2020-05-27 14:27:41 +0100

    update social tag details

--- src/server/controller/view.controller.ts
+++ src/server/controller/view.controller.ts
@@ -5,6 +5,10 @@ export const viewController = express.Router();
 
 const isDevMode = process.env.NODE_ENV === "development" || false;
 const isProdMode = process.env.NODE_ENV === "production" || false;
+const GA_ID = process.env.GA_ID || null;
+const FB_ID = process.env.FB_ID || null;
+const TYPEKIT_ID = process.env.TYPEKIT_ID || null;
+const TWITTER_PIXEL_ID = process.env.TWITTER_PIXEL_ID || null;
 
 let webpackManifest: any = {};
 if (isProdMode) {
@@ -16,8 +20,10 @@ viewController.use((req: express.Request, res: express.Response) => {
     const vendorsJSUrl:string = isProdMode ? webpackManifest["vendors.js"] : '/dist/vendors.bundle.js';
     const bundleJSUrl:string = isProdMode ? webpackManifest["main.js"] : '/dist/bundle.js';
     const title:string = 'h3nr7 :: A creative coder\'s journal';
-    const fbId:string = '1234567';
-    const typekitId:string = 'omu7wza';
+    const fbId:string = FB_ID;
+    const gaId: string = GA_ID;
+    const typekitId:string = TYPEKIT_ID;
+    const twitterPixelId:string = TWITTER_PIXEL_ID
 
     const og = {
         title,
@@ -29,8 +35,10 @@ viewController.use((req: express.Request, res: express.Response) => {
     res.render('home', { 
         layout: 'default',
         title,
+        gaId,
         fbId,
         typekitId,
+        twitterPixelId,
         og,
         vendorsJSUrl,
         bundleJSUrl
