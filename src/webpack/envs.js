c80a6f5f85657a15c1ac3816be7207c5df7ebe8f
Henry YP Ho <henryyp@monkiki.co>
2020-06-23 14:53:11 +0100

    update mistakes with social link

--- src/webpack/envs.js
+++ src/webpack/envs.js
@@ -1,7 +1,7 @@
 module.exports = {
     'process.env.LINKEDIN_SOCIAL_URL': JSON.stringify(process.env.LINKEDIN_SOCIAL_URL),
     'process.env.TWITTER_SOCIAL_URL': JSON.stringify(process.env.TWITTER_SOCIAL_URL),
-    'process.env.GTHUB_SOCIAL_URL': JSON.stringify(process.env.GTHUB_SOCIAL_URL),
+    'process.env.GITHUB_SOCIAL_URL': JSON.stringify(process.env.GITHUB_SOCIAL_URL),
     'process.env.INSTAGRAM_SOCIAL_URL': JSON.stringify(process.env.INSTAGRAM_SOCIAL_URL)
 
 }
